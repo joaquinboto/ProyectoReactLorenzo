cb73426e1cdd3d4ce4e7ccae45625bfa206c78cf
joaquinboto <joaquin.boto@hotmail.com>
2022-08-01 23:47:05 -0300

    asd

--- src/components/WhoIAm.jsx
+++ src/components/WhoIAm.jsx
@@ -39,17 +39,13 @@ const WhoIAm = () => {
                 <h1 className="h1Who">Who I am !?</h1>
                 <p className="pWho">Hi my name is Lorenzo and I am a quant. After studying Law and Finance at Argentina, I found out in programming my passion to bring into a reality my imagination. End up being a developer that grabs a necessity and see it as a opportunity to solve it.
                 </p>
+                <p className="pWho">When I design my code I keep in mind some standards: </p>
             </div>
             <div className="divCards">
-                <div>
-                    <p className="pWho">When I design my code I keep in mind some standards: </p>
-                </div>
-                <div>
                     <img src={card1} alt="" />
                     <img src={card2} alt="" />
                     <img src={card3} alt="" />
                     <img src={card4} alt="" />
-                </div>
             </div>
             <div className="divFooter">
                 <div className="footerWho">
